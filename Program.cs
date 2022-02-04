5a577210c81b14a640beb735cbe7fe0a86e0e356
Zeliha Nur UZTİMÜR <78546860+uztnur@users.noreply.github.com>
2022-02-04 14:16:51 +0300

    Delete Program.cs

--- Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("FRANZ KAFKA");
-//Ekrana çıktı vermek için kullanılır.
-Console.WriteLine("DONUSUM");
-//Ekrana çıktıyı yeni satırda vermek için kullanılır.
-Console.Read();
-// Çıktıyı enter tuşuna basana kadar ekranda tutar.
-
