1acedc4737f65ded54480ee46990211aaafeca45
Zeliha Nur UZTİMÜR <78546860+uztnur@users.noreply.github.com>
2022-02-05 19:27:29 +0300

    Ders-3

---
+++ Ders3.cs
@@ -0,0 +1,50 @@
+﻿/********** ÖRNEK-1 ************
+int sayi1, sayi2, sayi3, toplam; //int: sayısal ifadeleri tutmaya yarayan değişkendir.
+sayi1 = 4;
+sayi2 = 5;
+sayi3 = 6;
+toplam = sayi1 + sayi2 + sayi3;
+Console.WriteLine("TOPLAM: " + toplam);
+Console.Read();
+*/
+/********** ÖRNEK-2 ************
+int a, b, carpim;
+a = 6;
+b = 7;
+carpim = a * b;
+Console.WriteLine(" a*b= " + carpim);
+Console.Read();
+*/
+/********** ÖRNEK-3 ************
+int a, b, bolum;
+a = 10;
+b = 5;
+bolum = (a / b)/2;
+Console.Write("BÖLÜM= " + bolum);
+*/
+/********** ÖRNEK-4 ************
+string ad, soyad, numara;
+int sinav1, sinav2, sinav3, proje, ortalama;
+ad = "Nur";
+soyad = "UZT";
+numara = "2100";
+sinav1 = 80;
+sinav2 = 78;
+sinav3 = 78;
+proje = 78;
+ortalama = (sinav1 + sinav2 + sinav3 + proje) / 4;
+Console.WriteLine("******ÖĞRENCİ BİLGİLERİ ********");
+Console.WriteLine("AD = " + ad);
+Console.WriteLine("Soyad = " + soyad);
+Console.WriteLine("NUMARA = " + numara);
+Console.WriteLine();
+Console.WriteLine("****** NOT BİLGİLERİ ********");
+Console.WriteLine("SINAV 1= " + sinav1);
+Console.WriteLine("SINAV 2= " + sinav2);
+Console.WriteLine("SINAV 3= " + sinav3);
+Console.WriteLine("PROJE NOTU= " + proje);
+Console.WriteLine();
+Console.WriteLine("****** ORTALAMA BİLGİLERİ ********");
+Console.WriteLine("ORTALAMA = " + ortalama);
+Console.Read();
+*/
